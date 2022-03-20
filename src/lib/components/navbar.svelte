7b149fac87335fc803c48a4b15324adbd6dedd46
spences10 <spences10apps@gmail.com>
2022-03-20 12:50:39 +0000

    feat: :lipstick: add components

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,32 @@
+<script>
+  import ThemeSelect from './theme-select.svelte'
+
+  export let pages
+</script>
+
+<div class="navbar mb-10 shadow-lg bg-neutral text-neutral-content">
+  <div class="px-2 mx-2 navbar-start">
+    <a sveltekit:prefetch href="/">
+      <span class="text-lg font-bold"> KitQL with GraphCMS </span>
+    </a>
+  </div>
+  <div class="px-2 mx-2 navbar-center">
+    <div class="flex items-stretch">
+      <a
+        sveltekit:prefetch
+        class="btn btn-ghost btn-sm rounded-btn"
+        href={`/`}>Home</a
+      >
+      {#each pages as { title, slug }}
+        <a
+          sveltekit:prefetch
+          class="btn btn-ghost btn-sm rounded-btn"
+          href={`/${slug}`}>{title}</a
+        >
+      {/each}
+    </div>
+  </div>
+  <div class="navbar-end">
+    <ThemeSelect />
+  </div>
+</div>
